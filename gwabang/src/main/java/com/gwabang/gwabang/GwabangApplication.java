a04212813cd19bcc19b89d78b8aac212ead2415c
hungry404 <bdjg1029@gmail.com>
2025-05-11 18:58:47 +0900

    feat : 프로젝트 세팅
    docker 등등

---
+++ gwabang/src/main/java/com/gwabang/gwabang/GwabangApplication.java
@@ -0,0 +1,13 @@
+package com.gwabang.gwabang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwabangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwabangApplication.class, args);
+	}
+
+}
