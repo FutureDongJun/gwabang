a08f8c8aa4a38237eb0b83608f379135103c8aeb
FutureDongJun <kdjun1234@gmail.com>
2025-05-17 17:37:41 +0900

    대폭 수정

--- src/main/java/com/gwabang/gwabang/GwabangApplication.java
+++ src/main/java/com/gwabang/gwabang/GwabangApplication.java
@@ -3,12 +3,11 @@ package com.gwabang.gwabang;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.gwabang.gwabang.member.repository")
-@EntityScan(basePackages = "com.gwabang.gwabang.member.entity")
+@EnableJpaRepositories(basePackages = "com.gwabang.gwabang")
+@EntityScan(basePackages = "com.gwabang.gwabang")
 public class GwabangApplication {
 
 	public static void main(String[] args) {
