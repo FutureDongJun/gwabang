d6f7e8fc7e4c316dd18141c2e566642e3e2a0bef
AI-chemist97 <bdjg1029@gmail.com>
2025-05-26 21:27:29 +0900

    feat : 오타 수정 PostDetail

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sidebar from "./layout/Sidebar";
+import PostDetail from "./pages/PostDetail";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
@@ -12,7 +13,7 @@ function App() {
       <Routes>
         <Route element={<Sidebar />}>
           <Route index element={<HomePage />} />
-          <Route path="/article/:groupCode" element={<DepartmentBoard/>} />
+          <Route path="/article/:groupCode" element={<DepartmentBoard />} />
           <Route path="/article/:groupCode/:id" element={<PostDetail />} />
         </Route>
         <Route path="/login" element={<LoginPage />}></Route>
